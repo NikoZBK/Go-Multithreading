59a25776baa6a02f617bf91244b900d47a09ba07
NikoZBK <nikozbk@gmail.com>
2025-05-14 19:46:18 -0400

    https://medium.com/@brenomachadodomonte/multithreading-with-go-routines-in-golang-7e8fcd33be81

--- hello.go
+++ hello.go
@@ -8,14 +8,22 @@ import (
 func someTask(id int, data chan int) {
 	for taskId := range data {
 		time.Sleep(2 * time.Second)
-		fmt.Printf("Task %d executed\n", data)
+		fmt.Printf("Worker: %d executed Task %d\n", id, taskId)
 	}
 }
 
 func main() {
+	// Creating a channel
+	channel := make(chan int)
 
-	for i := range 10 {
-		someTask(i)
+	// Creating 10,000 workers to execute the task
+	for i := 0; i < 10000; i++ {
+		go someTask(i, channel)
+	}
+
+	// Filling channel with 100,000 numbers to be executed
+	for i := 0; i < 100000; i++ {
+		channel <- i
 	}
 
 }
